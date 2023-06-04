47b21013168e52325ad54764d3d305470605340d
janniks mac <jannik.loth@gmx.de>
2023-06-05 00:22:39 +0200

    created person class

--- 2_Objektorientierung/2_3_Vererbung/Person.hpp
+++ 2_Objektorientierung/2_3_Vererbung/Person.hpp
@@ -5,10 +5,32 @@
 #ifndef CPP_LECTURE_PERSON_HPP
 #define CPP_LECTURE_PERSON_HPP
 
+#include <string>
+
+using namespace std;
 
 class Person
 {
+public:
+    // Default Constructor
+    Person()
+    {};
 
+    // Overloaded Constructor
+    Person(string name, string adress, string email)
+        :   m_name(name)
+        ,   m_adress(adress)
+        ,   m_email(email)
+    {};
+
+    // Destructor
+    ~Person()
+    {};
+
+private:
+    string m_name;
+    string m_adress;
+    string m_email;
 };
 
 

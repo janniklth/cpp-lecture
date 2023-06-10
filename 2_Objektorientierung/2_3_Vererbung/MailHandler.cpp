a0c26751c74134d8672801c1936c463b98fc6b1b
janniks mac <jannik.loth@gmx.de>
2023-06-10 16:44:58 +0200

    added MailHandler.cpp and MailHandler.hpp

---
+++ 2_Objektorientierung/2_3_Vererbung/MailHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jannik Loth on 10.06.23.
+//
+
+#include "MailHandler.hpp"
---
+++ 2_Objektorientierung/2_3_Vererbung/MailHandler.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Jannik Loth on 10.06.23.
+//
+
+#ifndef CPP_LECTURE_MAILHANDLER_HPP
+#define CPP_LECTURE_MAILHANDLER_HPP
+
+
+class MailHandler
+{
+
+};
+
+
+#endif //CPP_LECTURE_MAILHANDLER_HPP

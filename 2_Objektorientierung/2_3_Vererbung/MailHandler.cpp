144a12e92e4947a83412e7e7ab5c94fbd94ec853
janniks mac <jannik.loth@gmx.de>
2023-06-11 08:54:48 +0200

    added functionality to writeMail()

--- 2_Objektorientierung/2_3_Vererbung/MailHandler.cpp
+++ 2_Objektorientierung/2_3_Vererbung/MailHandler.cpp
@@ -2,10 +2,15 @@
 // Created by Jannik Loth on 10.06.23.
 //
 
+#include <iostream>
 #include "MailHandler.hpp"
+
+using namespace std;
 
 // Function to send a message -> print some info about the message and the receiver
 void MailHandler::writeMail(Person receiver, string message)
 {
-
+    cout << "Send  mail to: " << receiver.getMName() << endl;
+    cout << " -> email: " << receiver.getMEmail() << endl;
+    cout << " -> adress: " << receiver.getMAdress() << endl;
 }

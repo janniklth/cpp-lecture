d857a36380e45f73ce8f7fb021b9fdf076003851
janniks mac <jannik.loth@gmx.de>
2023-06-12 17:41:28 +0200

    implemented and tested visit() method for normal place

--- 2_Objektorientierung/2_4_Polymorphismus/Place.cpp
+++ 2_Objektorientierung/2_4_Polymorphismus/Place.cpp
@@ -2,7 +2,10 @@
 // Created by Jannik Loth on 12.06.23.
 //
 
+#include <iostream>
 #include "Place.hpp"
+
+using namespace std;
 
 // getMethod -> returns the coordinates
 const coordinate &Place::getMCoordinates() const
@@ -19,5 +22,8 @@ const string &Place::getMName() const
 // method to visit a place (to be overwritten) -> print some info
 void Place::visit() const
 {
-
+    // print out the name and coordinates
+    cout << "Visiting " << this->getMName() << endl;
+    cout << " -> coordinates: (" << this->getMCoordinates().x << ", " << this->getMCoordinates().y << ")"
+         << endl;
 }
--- 2_Objektorientierung/2_4_Polymorphismus/main.cpp
+++ 2_Objektorientierung/2_4_Polymorphismus/main.cpp
@@ -4,8 +4,19 @@
 
 
 
+#include "Place.hpp"
+
 // main function
 int main()
 {
+    // initialise some places, sights and toilets
+    Place place1("Cafe", {2, 4});
+
+
+
+    // visit the place
+    place1.visit();
+
+
     return 0;
 }

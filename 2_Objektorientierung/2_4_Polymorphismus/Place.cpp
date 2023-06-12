48dd8e7cbb72c0b6f4d1660c1e35d06a03bcdc84
janniks mac <jannik.loth@gmx.de>
2023-06-12 16:30:23 +0200

    implemented functionality for Place()

--- 2_Objektorientierung/2_4_Polymorphismus/Place.cpp
+++ 2_Objektorientierung/2_4_Polymorphismus/Place.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Place.hpp"
+
+// getMethod -> returns the coordinates
+const coordinate &Place::getMCoordinates() const
+{
+    return m_coordinates;
+}
+
+// getMethod -> returns the name
+const string &Place::getMName() const
+{
+    return m_name;
+}
+
+// method to visit a place (to be overwritten) -> print some info
+void Place::visit() const
+{
+
+}
--- 2_Objektorientierung/2_4_Polymorphismus/Place.hpp
+++ 2_Objektorientierung/2_4_Polymorphismus/Place.hpp
@@ -1,3 +1,5 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "modernize-use-nodiscard"
 //
 // Created by Jannik Loth on 12.06.23.
 //
@@ -5,11 +7,51 @@
 #ifndef CPP_LECTURE_PLACE_HPP
 #define CPP_LECTURE_PLACE_HPP
 
+#include <string>
 
+using namespace std;
+
+// struct to store coordinates
+struct coordinate {
+    int x;
+    int y;
+};
+
+// base class
 class Place
 {
+public:
+    // Default Constructor
+    Place()
+    {};
+
+    // Overloaded Constructor
+    Place(string name, coordinate coordinates)
+        :   m_name(std::move(name))
+        , m_coordinates(coordinates)
+    {}
+
+    // Destructor
+    virtual ~Place()
+    = default;
+
+    // getMethod for name
+    const string &getMName() const;
+
+    // getMethod for coordinates
+    const coordinate &getMCoordinates() const;
+
+    // method to visit a place -> print info about that place
+    virtual void visit() const;
+
+
+protected:
+    string m_name;
+    coordinate m_coordinates;
 
 };
 
 
 #endif //CPP_LECTURE_PLACE_HPP
+
+#pragma clang diagnostic pop

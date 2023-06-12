22768d71a7dd2a8ba5457ef62ff01521d6131c7a
janniks mac <jannik.loth@gmx.de>
2023-06-12 18:28:59 +0200

    implemented and tested Sights class

--- 2_Objektorientierung/2_4_Polymorphismus/Sights.cpp
+++ 2_Objektorientierung/2_4_Polymorphismus/Sights.cpp
@@ -2,4 +2,20 @@
 // Created by Jannik Loth on 12.06.23.
 //
 
+#include <iostream>
 #include "Sights.hpp"
+
+using namespace std;
+
+// override method for visiting a place
+void Sights::visit() const
+{
+    Place::visit();
+    cout << " -> description: " << this->getMDescription() << endl;
+}
+
+// getMethod for description
+const string &Sights::getMDescription() const
+{
+    return m_description;
+}
--- 2_Objektorientierung/2_4_Polymorphismus/Sights.hpp
+++ 2_Objektorientierung/2_4_Polymorphismus/Sights.hpp
@@ -6,9 +6,42 @@
 #define CPP_LECTURE_SIGHTS_HPP
 
 
-class Sights
-{
+#include <utility>
 
+#include "Place.hpp"
+
+
+using namespace std;
+
+// derived class from Place
+class Sights : public Place
+{
+public:
+    // Default Constructor
+    Sights()
+    {};
+
+    // Overloaded Constructor
+    Sights(string name, coordinate coordinates, string description)
+        :   Place(std::move(name), coordinates)
+        ,   m_description(std::move(description))
+    {};
+
+
+    // Destructor
+    ~Sights()
+    {};
+
+
+    // getMethod for description
+    const string &getMDescription() const;
+
+    // override visit() to print all info
+    void visit() const override;
+
+
+private:
+    string m_description;
 };
 
 

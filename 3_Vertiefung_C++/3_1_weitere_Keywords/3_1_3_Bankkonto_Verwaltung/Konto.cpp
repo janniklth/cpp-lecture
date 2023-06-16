5a1a05acc3f8bb80ff4a016ac01e93d20eba9f07
janniks mac <jannik.loth@gmx.de>
2023-06-16 23:40:37 +0200

    added Konto.cpp and Konto.hpp

---
+++ 3_Vertiefung_C++/3_1_weitere_Keywords/3_1_3_Bankkonto_Verwaltung/Konto.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jannik Loth on 16.06.23.
+//
+
+#include "Konto.hpp"
---
+++ 3_Vertiefung_C++/3_1_weitere_Keywords/3_1_3_Bankkonto_Verwaltung/Konto.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Jannik Loth on 16.06.23.
+//
+
+#ifndef CPP_LECTURE_KONTO_HPP
+#define CPP_LECTURE_KONTO_HPP
+
+
+class Konto
+{
+
+};
+
+
+#endif //CPP_LECTURE_KONTO_HPP
